997124a434caf505ef47f4a2e882cbe0dc4448f6
robinplancon <robin.plancon@gmail.com>
2023-05-24 17:19:42 +0200

    add a form to game

--- js/app.js
+++ js/app.js
@@ -12,20 +12,20 @@ const display_dice = (result, player) => {
     const divDice = document.createElement("div");
     divDice.classList.add("dice");
     // ajout de la div créée dans la div player
-    if(player === "player"){
+    if (player === "player") {
         document.querySelector("#player").appendChild(divDice);
     } else if (player === "dealer") {
         document.querySelector("#dealer").appendChild(divDice);
     }
-    
+
     // modification du background position pour correspondre à la valeur du dé
     divDice.style.backgroundPositionX = `-${(result - 1) * 100}px`;
 };
 
-// fonction qui génèrent les lancers de dés prenant en paramètre 
+// fonction qui génèrent les lancers de dés prenant en paramètre
 // le nombre de dés à lancer
 const throw_dice = (number_of_dices = 1, player) => {
-    if (number_of_dices !== NaN){
+    if (number_of_dices !== NaN) {
         for (let i = 0; i < number_of_dices; i++) {
             display_dice(generate_number(MAX, MIN), player);
         }
@@ -34,20 +34,34 @@ const throw_dice = (number_of_dices = 1, player) => {
     }
 };
 
-const start_game = (opponent = false) => {
-    const number_of_dices = parseInt(prompt("Combien de dés voulez vous lancer ?"));
+const start_game = () => {
+    reset_display();
+    number_of_dices = diceNumber.value;
     throw_dice(number_of_dices, "player");
-    if (opponent) {
+    if (duo.checked) {
         const divOpponent = document.createElement("div");
         divOpponent.setAttribute("id", "dealer");
         divOpponent.classList.add("board");
         document.querySelector("#app").appendChild(divOpponent);
         throw_dice(number_of_dices, "dealer");
     }
-}
+};
 
-if(confirm("Voulez vous jouer contre le dealer ?")){
-    start_game(true);
-} else {
-    start_game();
-}
+const reset_display = () => {
+    const dealer = document.querySelector("#dealer");
+    if (dealer) {
+        dealer.remove();
+    }
+    document.querySelector("#player").replaceChildren();
+};
+
+const startButton = document.getElementById("start_game");
+const diceNumber = document.getElementById("number_of_dices");
+const duo = document.querySelector("#duo");
+
+startButton.addEventListener("click", start_game);
+document.addEventListener("keyup", (Event) => {
+    if(Event.key === "Enter") {
+        start_game();
+    }
+})
